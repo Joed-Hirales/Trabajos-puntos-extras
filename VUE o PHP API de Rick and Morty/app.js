e1d0d1d8e24f26d1daa670ca75fe68b649d4b3cb
Joed-Hirales <94884333+Joed-Hirales@users.noreply.github.com>
2024-11-28 18:05:15 -0700

    VUE o PHP: API de Clima

--- VUE o PHP API de Rick and Morty/app.js
+++ VUE o PHP API de Rick and Morty/app.js
@@ -1,27 +1,32 @@
-const { createApp } = Vue;
-
-createApp({
-  data() {
-    return {
-      characters: [],
-      nextPage: 'https://rickandmortyapi.com/api/character/'
-    };
+new Vue({
+  el: '#app',
+  data: {
+      apiKey: '14b098188aa6bb143f5f796fe6ef7757',
+      cities: ['La Paz', 'Cabo San Lucas', 'San José del Cabo', 'Loreto', 'Mulegé'],
+      citiesWeather: []
   },
   methods: {
-    async fetchCharacters() {
-      if (this.nextPage) {
-        try {
-          const response = await fetch(this.nextPage);
-          const data = await response.json();
-          this.characters.push(...data.results);
-          this.nextPage = data.info.next;
-        } catch (error) {
-          console.error('Error fetching characters:', error);
-        }
+      async fetchWeather() {
+          const units = 'metric';
+          const lang = 'es';
+          for (const city of this.cities) {
+              try {
+                  const response = await axios.get(
+                      `https://api.openweathermap.org/data/2.5/weather?q=${city},MX&units=${units}&lang=${lang}&appid=${this.apiKey}`
+                  );
+                  const { name, main, weather } = response.data;
+                  this.citiesWeather.push({
+                      name,
+                      temp: main.temp,
+                      weather: weather[0].description
+                  });
+              } catch (error) {
+                  console.error(`Error al conseguir el clima de ${city}:`, error);
+              }
+          }
       }
-    }
   },
-  mounted() {
-    this.fetchCharacters();
+  created() {
+      this.fetchWeather();
   }
-}).mount('#app');
+});
